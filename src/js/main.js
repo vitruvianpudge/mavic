0de52c66e957cf516eb77682a40bec5a23ea4cd4
Yura Mishchuk <yuramishchuk1@gmail.com>
2022-07-05 23:34:31 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// scroll page animation
+new fullpage('#fullpage', {
+});
+
+// slider
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
